210ab00ffd4f65c4f5066d4ff8f6de6754585834
Kareem Mohllal <kareem.mohllal@gmail.com>
2022-11-27 02:53:24 +0200

    Add oh-my-zsh.zsh file

---
+++ oh-my-zsh.zsh
@@ -0,0 +1,20 @@
+# oh-my-zsh theme
+ZSH_THEME="robbyrussell"
+
+# zsh-autosuggestions config
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=244"
+
+# oh-my-zsh plugins
+plugins=(
+	git
+	colorize
+	helm
+	fzf
+	zsh-syntax-highlighting
+	zsh-autosuggestions
+	kubectl
+	zsh-z
+	git-flow-completion
+	poetry
+	minikube
+)

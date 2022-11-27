4f7c83a315344225e54350fdcd3c671b719e06f8
Kareem Mohllal <kareem.mohllal@gmail.com>
2022-11-27 02:52:16 +0200

    Add aliases.zsh file

---
+++ aliases.zsh
@@ -0,0 +1,9 @@
+# fzf alias
+alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+
+# starship aliases
+alias gt='starship toggle gcloud disabled'
+alias ks='starship toggle kubernetes disabled'
+
+# bat alias
+alias cat="bat"
